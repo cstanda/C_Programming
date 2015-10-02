b430fa2150373496143086135fc5976557bf8458
cstanda <standa.inc@gmail.com>
2015-10-02 23:16:51 +0300

    2nd commit

---
+++ first_program/helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* int main() declares the main function /**/
+int main()
+{
+    /*printf sends output to standard out(screen) /**/
+    printf("Hello World!! This is output from my first program using C!\n");
+    
+    /* return 0
+    causes the function to return an of 0 (no error) 
+    to the shell that started the execution
+	/**/
+    return 0;
+}
